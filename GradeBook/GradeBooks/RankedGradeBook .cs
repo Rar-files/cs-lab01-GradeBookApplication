45a8f95f92893aacb0e3e311dee9165243a8b5da
Dominik Widlak <dominik.widlak@microsoft.wsei.edu.pl>
2020-10-10 20:07:16 +0200

    OverrideAllCalculateStatisticMethod

--- GradeBook/GradeBooks/RankedGradeBook .cs
+++ GradeBook/GradeBooks/RankedGradeBook .cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,41 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int countTop = 0;
+
+            if (Students.Count < 5) throw new InvalidOperationException("Number of students less than 5.");
+
+            foreach(Student x in Students)
+            {
+                if (x.AverageGrade >= averageGrade) countTop++;
+            }
+            int percyl = countTop / Students.Count * 100;
+
+            if (percyl > 80) return 'F';
+            else if (percyl > 60) return 'D';
+            else if (percyl > 40) return 'C';
+            else if (percyl > 20) return 'B';
+            else return 'A';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5) 
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 }
